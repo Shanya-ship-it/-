508121546c586ac308879949eadd82f56dbe7e66
nking <nking07049925@gmail.com>
2023-05-21 18:33:05 +0300

    minor codestyle fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -61,6 +61,7 @@ const App = () => {
     const res = await fetch("http://localhost:8080/test3", {
       method: "POST",
       headers: { "Content-type": "application/json" },
+      body: JSON.stringify({}),
     });
     // Парсим полученный JSON
     const json: User[] = await res.json();
